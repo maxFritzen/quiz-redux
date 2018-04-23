bc39bd1e01f7115b038b03bb8e2e86ecf3d121ad
maxFritzen <max.fritzen@hotmail.se>
2018-04-23 11:31:59 +0200

    modified loadingpage

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const LoadingPage = () => (
   <div className="loader">
-    <img className="loader__image" src="/images/ajax-loader-2.gif" />
+    <img className="loader__image" src="./images/ajax-loader-2.gif" />
   </div>
 );
 
